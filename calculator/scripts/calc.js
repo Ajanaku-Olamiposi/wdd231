75364e9d0d55a9e28c602df10618a2538590f83b
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-12-25 01:44:35 +0000

    update

---
+++ calculator/scripts/calc.js
@@ -0,0 +1,10 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date();
+currentyear.innerHTML = `${today.getFullYear()}`;
+const lastModified = document.querySelector("#lastmodified");
+const date = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modified: <span>${new Intl.DateTimeFormat("en-US").format(date)}</span>`;
+
+document.querySelector('.console').addEventListener('click', function() {
+    document.getElementById('hiddenInput').focus();
+});
